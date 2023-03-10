486bbaf511382f11e5f464dd0465f347cdf6b668
wanglei2351482230 <wl2351482230@163.com>
2023-03-10 17:48:06 +0800

    修改了初始默认界面

--- src/pages/mainPage.vue
+++ src/pages/mainPage.vue
@@ -41,7 +41,7 @@ export default {
   name: "mainPage",
   data() {
     return {
-      activeTab: 'AccuracyDifferPractice',
+      activeTab: 'GlyphDesign',
       distributionPractice: {
         isDemo: true,
         // 1分布实验，2微小差值实验
